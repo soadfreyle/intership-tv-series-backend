f29a9ba2a8c1371e220207d29de095e18ce7a843
Miguel Camargo <39164080+mcamar5@users.noreply.github.com>
2022-02-15 19:40:12 -0500

    Add Class Axios Service

---
+++ src/modules/core/service/request/axios.service.ts
@@ -0,0 +1,12 @@
+
+
+export class AxiosService {
+
+    public get () {
+        /**
+         * Importar axios
+         * Recibir endpoint como parametro del metodo
+         * utilizar axios.get para servicios get
+         */
+    }
+}
